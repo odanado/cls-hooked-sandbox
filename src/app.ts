5853c2ec3443161bf6f44ad6b3e07a1789ce14ce
odan <odan3240@gmail.com>
2021-06-02 02:25:47 +0900

    request id がログにでるようにした
    ref: https://www.grandream.jp/blog/lambda-logger/

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,42 @@
-import express from "express";
+import express, { RequestHandler } from "express";
+import cls from "cls-hooked";
+
+const nsid = "request";
+const ns = cls.createNamespace(nsid);
 
 const app = express();
 
+app.use((_, __, next) => {
+  ns.run(() => next());
+});
+
+let id = 0;
+app.use((_, __, next) => {
+  if (ns && ns.active) {
+    ns.set("id", id++);
+  }
+  next();
+});
+
+function log(message: string): void {
+  const id = ns.get("id");
+  console.log({
+    id,
+    message,
+    time: new Date().getTime(),
+  });
+}
+
 app.get("/", (_, res) => {
-  res.send("poyo");
+  log("start");
+  setTimeout(() => {
+    log("end");
+    res.send("poyo");
+  }, 1000 * Math.random());
 });
 
 async function main(): Promise<void> {
-  app.listen(3000);
+  app.listen(3000, () => console.log("http://localhost:3000"));
 }
 
 main();
